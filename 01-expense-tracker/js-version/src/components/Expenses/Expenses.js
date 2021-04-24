499f70df8b4891e8d879bc84f487ee9e4a6e3267
Felipe Sanches <sanchesfelipe153@gmail.com>
2021-04-24 14:46:18 -0300

    Fim da Seção 3: React Basics & Working with Components

---
+++ 01-expense-tracker/js-version/src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import Card from '../UI/Card'
+import ExpenseItem from './ExpenseItem'
+
+import './Expenses.css'
+
+const Expenses = (props) => {
+	return (
+		<Card className="expenses">
+			{props.items.map((item) => (
+				<ExpenseItem
+					key={item.id}
+					title={item.title}
+					amount={item.amount}
+					date={item.date}
+				/>
+			))}
+		</Card>
+	)
+}
+
+export default Expenses
